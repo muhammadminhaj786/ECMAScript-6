7613e4c2afb7b08def65fee7bffc4baff8b7ec94
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-07-20 09:41:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -179,4 +179,17 @@ async function foo() {
     }
 foo() 
 
+// find the length of a string
+const len = string.length;
+
+// loop through half of the string
+for (let i = 0; i < len / 2; i++) {
+
+    // check if first and last string are same
+    if (string[i] !== string[len - 1 - i]) {
+        return 'It is not a palindrome';
+    }
+}
+return 'It is a palindrome';
+
          
