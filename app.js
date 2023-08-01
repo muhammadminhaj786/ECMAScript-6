f376c3600bb2772fc865d81e7d33eeaeb3a8c002
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-08-01 21:07:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -280,3 +280,12 @@ function factorialize(num) {
 
 factorialize(5);
 
+// reverse strings
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+reverseString('hello');
